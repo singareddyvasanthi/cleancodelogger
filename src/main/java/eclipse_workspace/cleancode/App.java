2a301f0bc7fff20c0a4b125d13995b3a99276a97
vasanthi <singareddyvasanthi@gmail.com>
2020-02-27 23:53:14 +0530

    first commit

--- src/main/java/eclipse_workspace/cleancode/App.java
+++ src/main/java/eclipse_workspace/cleancode/App.java
@@ -2,24 +2,27 @@ package eclipse_workspace.cleancode;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App 
 {
-	
+	private static final Logger log = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
         Scanner sc = new Scanner(System.in);
         double p,t,r;
-        System.out.println("Enter the amount");
+        log.info("Enter the amount");
         p = sc.nextDouble();
-        System.out.println("Enter the time");
+        log.info("Enter the time");
         t = sc.nextDouble();
-        System.out.println("Enter the intrest");
+        log.info("Enter the intrest");
         r = sc.nextDouble();
         intrest obj = new intrest();
         double si = obj.simpleIntrest(p,t,r);
         double ci = obj.compoundIntrest(p,t,r);
-        System.out.println("Simple Intrest is "+si);
-        System.out.println("Compound Intrest is "+ci);
+        log.info("Simple Intrest is "+si);
+        log.info("Compound Intrest is "+ci);
         
         
         
--- src/main/java/eclipse_workspace/cleancode/construction.java
+++ src/main/java/eclipse_workspace/cleancode/construction.java
@@ -2,7 +2,11 @@ package eclipse_workspace.cleancode;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class construction {
+	private static final Logger log = LogManager.getLogger(construction.class);
 
 	public construction() {
 		// TODO Auto-generated constructor stub
@@ -10,12 +14,12 @@ public class construction {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Enter the area of house");
+		log.info("Enter the area of house");
         double feet = sc.nextInt();
-        System.out.println("If you want a home with Standarad Materials then press 1");
-        System.out.println("If you want a home with above Standarad Materials then press 2");
-        System.out.println("If you want a home with high Standarad Materials then press 3");
-        System.out.println("If you want a home with high Standarad Materials and fully automated home then press 4");
+        log.info("If you want a home with Standarad Materials then press 1");
+        log.info("If you want a home with above Standarad Materials then press 2");
+        log.info("If you want a home with high Standarad Materials then press 3");
+        log.info("If you want a home with high Standarad Materials and fully automated home then press 4");
 
         int op = sc.nextInt();
         materials obj2 = new materials();
@@ -29,10 +33,10 @@ public class construction {
            break;
         case 4: ans = obj2.hfsm(feet);
            break;
-        default:  System.out.printf("Error! Enter correct operator");
+        default:  log.info("Error! Enter correct operator");
            return;
      }
-        System.out.println("The total construction cost is "+ans);
+        log.info("The total construction cost is "+ans);
 
 
 	}
